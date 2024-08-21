17e5e394e79d6f45983e7a9ba73b3571b9f8d3b1
Satwik Kar <satwik.k.2000@gmail.com>
2024-08-21 21:50:42 +0530

    commit

--- Frontend/Dropdown/src/App.jsx
+++ Frontend/Dropdown/src/App.jsx
@@ -3,11 +3,12 @@ import {BrowserRouter} from 'react-router-dom'
 import './App.css'
 
 import  Chatbot  from './components/ChatBot/ChatBot.jsx'
+import Dropdown from "./components/Dropdownn.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
 
-return <Chatbot/>
+return <Dropdown/>
 }
 
 export default App
